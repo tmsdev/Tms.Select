b4dbba9ab619dbfdd902e806a38ba1e87f10ebfa
Joachim Beck <beck@tms-institut.de>
2021-10-18 11:44:05 +0200

    BUGFIX: use correct content context in Flow queries, add new feature to prefix labels by their node context (hidden, not in menus, not live, removed)

--- Classes/DataSource/NodeDataDataSource.php
+++ Classes/DataSource/NodeDataDataSource.php
@@ -5,24 +5,16 @@ use Neos\Eel\FlowQuery\FlowQuery;
 use Neos\Media\Domain\Model\ImageInterface;
 use Neos\Media\Domain\Model\ThumbnailConfiguration;
 use Neos\Media\Domain\Service\AssetService;
-use Neos\Neos\Domain\Service\ContentContext;
-use Neos\Neos\Domain\Service\ContentContextFactory;
 use Neos\Neos\Service\DataSource\AbstractDataSource;
 use Neos\ContentRepository\Domain\Model\NodeInterface;
 use Neos\Flow\Annotations as Flow;
 
-class NodeDataDataSource extends AbstractDataSource {
-
+class NodeDataDataSource extends AbstractDataSource
+{
     /**
      * @var string
      */
     static protected $identifier = 'tms-select-nodedata';
-
-    /**
-     * @Flow\Inject
-     * @var ContentContextFactory
-     */
-    protected $contentContextFactory;
 
     /**
      * @Flow\Inject
@@ -39,15 +31,13 @@ class NodeDataDataSource extends AbstractDataSource {
      */
     public function getData(NodeInterface $node = NULL, array $arguments = [])
     {
-        /** @var ContentContext $contentContext */
-        $contentContext = $this->contentContextFactory->create(array($node));
-
         if (isset($arguments['startingPoint']))
-            $rootNode = $contentContext->getNode($arguments['startingPoint']);
+            $rootNode = $node->getContext()->getNode($arguments['startingPoint']);
         else
-            $rootNode = $contentContext->getRootNode();
+            $rootNode = $node->getContext()->getRootNode();
 
         $q = new FlowQuery(array($rootNode));
+        $q = $q->context(['invisibleContentShown' => true, 'removedContentShown' => true, 'inaccessibleContentShown' => true]);
         $nodes = array();
 
         if (!isset($arguments['nodeType']) && !isset($arguments['nodeTypes']))
@@ -57,6 +47,9 @@ class NodeDataDataSource extends AbstractDataSource {
         if (isset($arguments['nodeTypes']))
             $nodeTypes = $arguments['nodeTypes'];
 
+        $setLabelPrefixByNodeContext = false;
+        if (isset($arguments['setLabelPrefixByNodeContext']) && $arguments['setLabelPrefixByNodeContext'] == true)
+            $setLabelPrefixByNodeContext = true;
         $labelPropertyName = null;
         if (isset($arguments['labelPropertyName']))
             $labelPropertyName = $arguments['labelPropertyName'];
@@ -66,10 +59,10 @@ class NodeDataDataSource extends AbstractDataSource {
 
         if (isset($arguments['groupBy'])) {
             foreach ($q->find('[instanceof ' . $arguments['groupBy'] . ']')->get() as $parentNode) {
-                $nodes = array_merge($nodes, $this->getNodes($parentNode, $nodeTypes, $labelPropertyName, $previewPropertyName, $arguments['groupBy']));
+                $nodes = array_merge($nodes, $this->getNodes($parentNode, $nodeTypes, $labelPropertyName, $previewPropertyName, $setLabelPrefixByNodeContext, $arguments['groupBy']));
             }
         } else {
-            $nodes = $this->getNodes($rootNode, $nodeTypes, $labelPropertyName, $previewPropertyName);
+            $nodes = $this->getNodes($rootNode, $nodeTypes, $labelPropertyName, $previewPropertyName, $setLabelPrefixByNodeContext);
         }
 
         return $nodes;
@@ -80,13 +73,15 @@ class NodeDataDataSource extends AbstractDataSource {
      * @param array $nodeTypes
      * @param string|null $labelPropertyName
      * @param string|null $previewPropertyName
+     * @param boolean $labelPrefixNodeContext
      * @param string|null $groupBy
      *
      * @return array
      */
-    protected function getNodes(NodeInterface $parentNode, $nodeTypes, $labelPropertyName = null, $previewPropertyName = null, $groupBy = null)
+    protected function getNodes(NodeInterface $parentNode, $nodeTypes, $labelPropertyName = null, $previewPropertyName = null, $setLabelPrefixByNodeContext = false, $groupBy = null)
     {
         $q = new FlowQuery(array($parentNode));
+        $q = $q->context(['invisibleContentShown' => true, 'removedContentShown' => true, 'inaccessibleContentShown' => true]);
         $nodes = array();
 
         $filter = [];
@@ -107,14 +102,46 @@ class NodeDataDataSource extends AbstractDataSource {
                         continue;
                     $preview = $thumbnail['src'];
                 }
+
+                $label = $labelPropertyName ? $node->getProperty($labelPropertyName) : $node->getLabel();
+                $groupLabel = $parentNode->getLabel();
+
+                if ($setLabelPrefixByNodeContext) {
+                    $label = $this->getLabelPrefixByNodeContext($node, $label);
+                    $groupLabel = $this->getLabelPrefixByNodeContext($parentNode, $groupLabel);
+                }
+
                 $nodes[] = array(
                     'value' => $node->getIdentifier(),
-                    'label' => $labelPropertyName ? $node->getProperty($labelPropertyName) : $node->getLabel(),
-                    'group' => ($groupBy !== null ? $parentNode->getLabel() : null),
+                    'label' => $label,
+                    'group' => ($groupBy !== null ? $groupLabel : null),
                     'preview' => $preview
                 );
             }
         }
         return $nodes;
+    }
+
+    /**
+     * @param NodeInterface $node
+     * @param string $label
+     *
+     * @return string
+     */
+    protected function getLabelPrefixByNodeContext(NodeInterface $node, string $label)
+    {
+        if ($node->isHidden())
+            $label = '[HIDDEN] ' . $label;
+        if ($node->isRemoved())
+            $label = '[REMOVED] ' . $label;
+        if ($node->isHiddenInIndex())
+            $label = '[NOT IN MENUS] ' . $label;
+
+        $q = new FlowQuery(array($node));
+        $nodeInLiveWorkspace = $q->context(['workspaceName' => 'live'])->get(0);
+        if (!$nodeInLiveWorkspace instanceof NodeInterface)
+            $label = '[NOT LIVE] ' . $label;
+
+        return $label;
     }
 }
--- README.md
+++ README.md
@@ -26,14 +26,16 @@ composer require tms/select
               groupBy: 'Your.Package:GroupType'
               startingPoint: '/start/here/instead/of/rootnode'
               labelPropertyName: 'title'
+              setLabelPrefixByNodeContext: true
               previewPropertyName: 'thumbnailImage' # works with Neos 7.2+
 ```
 
 ### Optional parameters
-| Parameter name        | Description |
-|-----------------------|---|
-| `labelPropertyName`   | Choose your specific **text property name** - if not set, the nodes label will be used. |
-| `previewPropertyName` | Choose your specific **image property name** to display a custom preview icon as mentioned in the [Neos 7.2 release notes](https://www.neos.io/blog/neos-flow-72-released.html#neos-7-1-features). |
+| Parameter name                | Description |
+|-------------------------------|---|
+| `labelPropertyName`           | Choose your specific **text property name** - if not set, the nodes label will be used. |
+| `setLabelPrefixByNodeContext` | If set to `true`, labels get prefixed by `[HIDDEN] ...`, `[NOT IN MENUS] ...`, `[NOT LIVE] ...` and `[REMOVED] ...` depending on the node context. |
+| `previewPropertyName`         | Choose your specific **image property name** to display a custom preview icon as mentioned in the [Neos 7.2 release notes](https://www.neos.io/blog/neos-flow-72-released.html#neos-7-1-features). |
 
 ## Acknowledgments
 Development sponsored by [tms.development - Online Marketing and Neos CMS Agency](https://www.tms-development.de/)
